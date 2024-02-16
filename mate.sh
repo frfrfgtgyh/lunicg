8ef61773e5e68edaf530954e1aab515d0de7f4aa
frfrfgtgyh <160009200+frfrfgtgyh@users.noreply.github.com>
2024-02-16 11:55:00 +0100

    Update mate.sh

--- mate.sh
+++ mate.sh
@@ -1,6 +1,6 @@
-rm -rf ngrok  ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+rm -rf ngrok  ngrok-v3-stable-linux-amd64.zip > /dev/null 2>&1
 echo "Download ngrok"
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
